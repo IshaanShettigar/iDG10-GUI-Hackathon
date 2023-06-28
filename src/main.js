c432e9f45e7566447953d293255509d7d0b2faa1
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-06-28 22:43:44 +0530

    showLinkSettings

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import { subseaSeparator, subseaPump, UTA, productionWellST, injectionWellST, ma
 import { assignCustomParams } from "./element-attrs.js"
 import { saveGraph, openFile, fixFormat, saveAsPNG } from "./persist.js"
 import { displayHighlight, removeHighlight, pasteElement, addToolsOnFileLoad, elementToolsMapping, addElementTools, addlinkTools } from "./utils.js"
-import { showLinkSettings } from './showLinkSettings.js';
 
 
 /** Defining fsm global variable for element undo redo */
@@ -1043,6 +1042,59 @@ mainPaper.on("blank:pointerclick", function () {
 // creating a custom button
 export var selectedLinkView = null;
 export const connectorSettingsWrapper = document.getElementById('connector-settings-wrapper')
+
+/**
+ * @constant {joint.dia.linkTools} showLinkSettings
+ * Defines the button that will trigger the connector settings popup on the left hand side when clicked
+ */
+var showLinkSettings = joint.linkTools.Button.extend({
+  name: "show-link-settings",
+  options: {
+    markup: [
+      {
+        tagName: "circle",
+        selector: "outer",
+        attributes: {
+          r: 9,
+          fill: "#ffffff",
+          cursor: "pointer",
+        },
+      },
+      {
+        tagName: "circle",
+        selector: "button",
+        attributes: {
+          r: 7,
+          fill: "#000000",
+          cursor: "pointer",
+        },
+      },
+      {
+        tagName: "path",
+        selector: "icon",
+        attributes: {
+          d: "M -2 4 2 4 M 0 3 0 0 M -2 -1 1 -1 M -1 -4 1 -4",
+          fill: "none",
+          stroke: "#FFFFFF",
+          "stroke-width": 2,
+          "pointer-events": "none",
+        },
+      },
+    ],
+    distance: "50%",
+    offset: 0,
+    action: function (evt, linkView, buttonView) {
+      // console.log(linkView.model.attributes.attrs)
+      connectorSettingsWrapper.classList.add('is-active')
+      populateConnectorSettings(linkView.model)
+      elementSettingsWrapper.classList.remove('is-active')
+      selectedLinkView = linkView;
+      removeHighlight(mainGraph, mask, mainPaper)
+      // highlight the link
+      displayLinkHighlight(linkView, mainGraph, mask, mainPaper)
+    },
+  },
+});
 
 mainPaper.on('link:connect', (linkView, evt, elementViewConnected, magnet) => {
   var verticesTool = new joint.linkTools.Vertices();
