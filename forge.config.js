b8874f96a9d03a88c7c88637588d9241f8de401c
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-06-30 00:06:46 +0530

    forge

--- forge.config.js
+++ forge.config.js
@@ -1,20 +1,22 @@
-export const packagerConfig = {
-    icon: './public/icons/icon.ico'
-};
-export const makers = [
-    {
-        name: '@electron-forge/maker-dmg'
-    }
-];
-export const publishers = [
-    {
-        name: '@electron-forge/publisher-github',
-        config: {
-            repository: {
-                owner: 'Ishaan Shettigar',
-                name: 'iDG10-GUI-Hackathon'
-            },
-            prerelease: true
+module.exports = {
+    packagerConfig: {
+        icon: './public/icons/icon.ico'
+    },
+    makers: [
+        {
+            name: '@electron-forge/maker-dmg'
         }
-    }
-];
+    ],
+    publishers: [
+        {
+            name: '@electron-forge/publisher-github',
+            config: {
+                repository: {
+                    owner: 'Ishaan Shettigar',
+                    name: 'iDG10-GUI-Hackathon'
+                },
+                prerelease: true
+            }
+        }
+    ]
+};
