e01404c8e706b7c8004982372a624f91f5a1111c
Ishaan Shettigar <ishaanshettigar@gmail.com>
2023-10-08 18:32:43 +0530

    on element hover modal is displayed

--- src/main.js
+++ src/main.js
@@ -1025,7 +1025,15 @@ const elementSettingsWrapper = document.getElementById('element-settings-wrapper
 var selectedCellView = null;
 // Adding element highlighting
 var mask = joint.highlighters.mask;
+
+let isHighlighted = false;
+
 mainPaper.on("element:pointerclick", function (cellView) {
+  isHighlighted = true;
+  /* To deal with the on hover MODAL */
+  hoverContainer.classList.add('hidden')
+  isHovering = false;
+  /* -------------------------------- */
 
   selectedCellView = displayHighlight(cellView, mainGraph, mask, mainPaper)
   populateElementSettings(cellView.model)
@@ -1039,6 +1047,13 @@ mainPaper.on("element:pointerclick", function (cellView) {
 
 // Remove element highlighting
 mainPaper.on("blank:pointerclick", function () {
+  /* To deal with the on hover MODAL */
+  hoverContainer.classList.add('hidden')
+  isHovering = false;
+  /* -------------------------------- */
+
+  isHighlighted = false;
+
   // Remove all Highlighters from all cells
   console.log("remove highlight");
   removeHighlight(mainGraph, mask, mainPaper)
@@ -1695,6 +1710,10 @@ var undoJSON = {
  */
 
 mainPaper.on('element:pointerdown', (cellView) => {
+  /* To deal with the on hover MODAL */
+  hoverContainer.classList.add('hidden')
+  isHovering = false;
+  /* -------------------------------- */
   if (STATE == 0) {
     STATE = 1;
     undoJSON["oldPosition"] = cellView.model.position()
@@ -1910,3 +1929,76 @@ function pan(event) {
 mainPaper.on("blank:pointerdown", panStart)
 mainPaper.on("cell:pointerup blank:pointerup", panStop)
 $("#main-paper-div").mousemove(pan);
+
+
+/* on hover modal/tooltip */
+let hoverContainer = document.getElementById('tooltip-container-id')
+
+let modalCompName = document.getElementById('cname');
+
+let hoverP1 = document.getElementById('p1')
+let hoverP2 = document.getElementById('p2')
+let hoverP3 = document.getElementById('p3')
+let hoverP4 = document.getElementById('p4')
+let hoverP5 = document.getElementById('p5')
+
+let hoverV1 = document.getElementById('v1')
+let hoverV2 = document.getElementById('v2')
+let hoverV3 = document.getElementById('v3')
+let hoverV4 = document.getElementById('v4')
+let hoverV5 = document.getElementById('v5')
+
+
+
+let isHovering = false;
+mainPaper.on("element:mouseenter", (cellView, evt) => {
+  if (isHovering != true && isHighlighted == false) {
+    isHovering = true;
+  }
+  setTimeout(() => {
+    if (isHovering == true) {
+      let modelAttrs = cellView.model.attributes.attrs /* There are multiple declarations of modelAttrs will that affect? maybe based on the diff between let and var maybe some problem in the future */
+      hoverV1.textContent = modelAttrs["parameter1"]
+      hoverV2.textContent = modelAttrs["parameter2"]
+      hoverV3.textContent = modelAttrs["parameter3"]
+      hoverV4.textContent = modelAttrs["parameter4"]
+      hoverV5.textContent = modelAttrs["parameter5"]
+
+      modalCompName.textContent = cellView.model.attributes.type;
+
+      hoverContainer.classList.remove('hidden')
+
+      let hoverX = evt["originalEvent"].clientX + 20;
+      let hoverY = evt["originalEvent"].clientY + 20;
+      console.log(`HoverX ${hoverX} HoverY ${hoverY} Window Height ${window.innerHeight} Width ${window.innerWidth}`);
+      let modelSize = cellView.model.attributes.size;
+      // The below if conditions handle the edges of having to display the modal when on the edges of the screen so that the modal is still visible
+      if (hoverY + 160 > window.innerHeight) {
+        hoverContainer.style.top = `${hoverY - 4 * modelSize.height}px`
+        hoverContainer.style.left = `${hoverX}px`
+        console.log("on hover hieght exceeded");
+      }
+      else if (hoverX + 160 > window.innerWidth) {
+        hoverContainer.style.top = `${hoverY}px`
+        hoverContainer.style.left = `${hoverX - 2.2 * modelSize.width}px`
+      }
+      else {
+        hoverContainer.style.top = `${hoverY}px`
+        hoverContainer.style.left = `${hoverX}px`
+      }
+    }
+  }, 500)
+  // displayOnHover(cellView, evt)
+})
+
+mainPaper.on("element:mouseout", (cellView, evt) => {
+  isHovering = false;
+  hoverContainer.classList.add('hidden')
+})
+
+mainPaper.on("element:pointerdblclick", (cellView, evt) => {
+  /* To deal with the on hover MODAL */
+  hoverContainer.classList.add('hidden')
+  isHovering = false;
+  /* -------------------------------- */
+})
