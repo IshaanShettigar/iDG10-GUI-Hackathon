538683f8c5169bfe19cba4c5c67f387262aa524d
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-10-13 15:10:56 +0530

    drawing

--- apps/app-jointjs/app/page.tsx
+++ apps/app-jointjs/app/page.tsx
@@ -1,3 +1,56 @@
+"use client";
+
+import { useState, useCallback } from "react";
+import ReactFlow, {
+  Controls,
+  Background,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  Edge,
+  Node,
+  OnConnect,
+  OnEdgesChange,
+  OnNodesChange,
+} from "reactflow";
+import "reactflow/dist/style.css";
+
+const initialNodes: Node[] = [
+  { id: "1", data: { label: "Hello" }, position: { x: 5, y: 25 } },
+  { id: "2", data: { label: "World" }, position: { x: 100, y: 100 } },
+];
+
+const initialEdges: Edge[] = [];
+
 export default function Home() {
-  return <h1>Hi</h1>;
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div className="flow" style={{ height: "100vh", width: "100vw" }}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 }
